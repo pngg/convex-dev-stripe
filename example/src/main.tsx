a1d53cfaad2b1a51e760e0b092fee25125e7e982
Michael Shimeles <michaelwasihun96@gmail.com>
2025-11-26 22:58:24 -0500

    Add analytics

--- example/src/main.tsx
+++ example/src/main.tsx
@@ -5,6 +5,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
+import { Analytics } from "@vercel/analytics/next"
 
 const address = import.meta.env.VITE_CONVEX_URL;
 
@@ -22,6 +23,7 @@ createRoot(document.getElementById("root")!).render(
     <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
       <ConvexProviderWithClerk client={convex} useAuth={useAuth}>
         <App />
+        <Analytics />
       </ConvexProviderWithClerk>
     </ClerkProvider>
   </StrictMode>
